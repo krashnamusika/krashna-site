7e10beb7a730ae4e6b959b952574b695d1fe3b9b
Georgios Andreadis <g.andreadis@student.tudelft.nl>
2018-02-07 21:09:12 +0100

    Locally include scripts

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,6 +2,9 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import '../utils/i18n.js'
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../../node_modules/font-awesome/css/font-awesome.min.css'
+import '../../node_modules/bootstrap/dist/js/bootstrap.min.js'
 
 import Header from '../components/Header'
 import Footer from '../components/Footer'
@@ -34,33 +37,31 @@ class TemplateWrapper extends React.Component {
                 'krashna, krashna musika, dsmg krashna, delft, muziek, music, orchestra, choir, tu delft',
             },
           ]}
-          script={[
-            {
-              src:
-                'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js',
-            },
-            {
-              src:
-                'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js',
-            },
-          ]}
+          script={
+            [
+              // {
+              //   src:
+              //     'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js',
+              // },
+              // {
+              //   src:
+              //     'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js',
+              // },
+            ]
+          }
         >
-          <link
+          {/* <link
             rel="stylesheet"
             href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-            integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-            crossorigin="anonymous"
-          />
+          /> */}
           <link
             href="https://fonts.googleapis.com/css?family=Raleway"
             rel="stylesheet"
           />
-          <link
+          {/* <link
             href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
             rel="stylesheet"
-            integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
-            crossorigin="anonymous"
-          />
+          /> */}
           <link rel="shortcut icon" type="image/png" href={favicon} />
         </Helmet>
         <Header />
