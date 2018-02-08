cf21e5a967a2b11f0414bfd526ef4519c921119e
Georgios Andreadis <g.andreadis@student.tudelft.nl>
2018-02-08 08:56:22 +0100

    Move Font-Awesome dependency to CDN

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,7 +4,6 @@ import Helmet from 'react-helmet'
 import '../utils/i18n.js'
 
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../../node_modules/font-awesome/css/font-awesome.min.css'
 
 import Header from '../components/Header'
 import Footer from '../components/Footer'
