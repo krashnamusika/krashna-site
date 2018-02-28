2d808a99f0d38fef3faa39dfdaf95db14b60614a
Georgios Andreadis <g.andreadis@student.tudelft.nl>
2018-02-28 10:48:01 +0100

    Fix translation directory issue

--- gatsby-node.js
+++ gatsby-node.js
@@ -12,6 +12,9 @@ exports.onPostBootstrap = () => {
   console.log("Copying locales");
   const nlTranslation = yaml.load(fs.readFileSync(path.join(__dirname, "/src/locales/nl-NL/translations.yaml"), {encoding: 'utf-8'}));
   const enTranslation = yaml.load(fs.readFileSync(path.join(__dirname, "/src/locales/en-GB/translations.yaml"), {encoding: 'utf-8'}));
+  fs.existsSync(path.join(__dirname, "/public/locales")) || fs.mkdirSync(path.join(__dirname, "/public/locales"));
+  fs.existsSync(path.join(__dirname, "/public/locales/nl-NL")) || fs.mkdirSync(path.join(__dirname, "/public/locales/nl-NL"));
+  fs.existsSync(path.join(__dirname, "/public/locales/en-GB")) || fs.mkdirSync(path.join(__dirname, "/public/locales/en-GB"));
   fs.writeFileSync(path.join(__dirname, "/public/locales/nl-NL/translations.json"), JSON.stringify(nlTranslation));
   fs.writeFileSync(path.join(__dirname, "/public/locales/en-GB/translations.json"), JSON.stringify(enTranslation));
 
