4dfac0f133c7d2be45ff1431d57eede1c139e0db
Georgios Andreadis <g.andreadis@student.tudelft.nl>
2018-03-02 21:00:21 +0100

    Split translations into smaller files

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,18 +10,22 @@ const yaml = require("js-yaml");
 
 exports.onPostBootstrap = () => {
   console.log("Copying locales");
-  const nlTranslation = yaml.load(fs.readFileSync(path.join(__dirname, "/src/locales/nl-NL/translations.yaml"), {encoding: 'utf-8'}));
-  const enTranslation = yaml.load(fs.readFileSync(path.join(__dirname, "/src/locales/en-GB/translations.yaml"), {encoding: 'utf-8'}));
+
+  const nlTranslation = loadTranslationObject("nl-NL");
+  const enTranslation = loadTranslationObject("en-GB");
+
+  // Create directory structure
   fs.existsSync(path.join(__dirname, "/public/locales")) || fs.mkdirSync(path.join(__dirname, "/public/locales"));
-  fs.existsSync(path.join(__dirname, "/public/locales/nl-NL")) || fs.mkdirSync(path.join(__dirname, "/public/locales/nl-NL"));
-  fs.existsSync(path.join(__dirname, "/public/locales/en-GB")) || fs.mkdirSync(path.join(__dirname, "/public/locales/en-GB"));
+  fs.existsSync(path.join(__dirname, "/public/locales/nl-NL")) ||
+  fs.mkdirSync(path.join(__dirname, "/public/locales/nl-NL"));
+  fs.existsSync(path.join(__dirname, "/public/locales/en-GB")) ||
+  fs.mkdirSync(path.join(__dirname, "/public/locales/en-GB"));
+
+  // Save bundled translation files
   fs.writeFileSync(path.join(__dirname, "/public/locales/nl-NL/translations.json"), JSON.stringify(nlTranslation));
   fs.writeFileSync(path.join(__dirname, "/public/locales/en-GB/translations.json"), JSON.stringify(enTranslation));
 
-  // fs.copySync(
-  //   path.join(__dirname, "/src/locales"),
-  //   path.join(__dirname, "/public/locales")
-  // );
+  // Create fallbacks for country-specific codes
   fs.copySync(
     path.join(__dirname, "/public/locales/en-GB"),
     path.join(__dirname, "/public/locales/en")
@@ -31,3 +35,12 @@ exports.onPostBootstrap = () => {
     path.join(__dirname, "/public/locales/nl")
   );
 };
+
+
+function loadTranslationObject(languageCode) {
+  const srcPath = path.join(__dirname, `/src/locales/${languageCode}/`);
+  const translationObjects = fs.readdirSync(srcPath).map(file => (
+    yaml.load(fs.readFileSync(path.join(srcPath, file)), {encoding: "utf-8"})
+  ));
+  return Object.assign({}, ...translationObjects)
+}
