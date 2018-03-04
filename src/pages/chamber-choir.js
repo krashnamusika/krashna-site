928efd87d239d7504324586ae51786295ffd6264
Georgios Andreadis <g.andreadis@student.tudelft.nl>
2018-03-04 16:47:48 +0100

    Add chamber choir page

---
+++ src/pages/chamber-choir.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Markdown from 'react-remarkable'
+import CenteredImage from "../components/CenteredImage";
+import PageTemplate from '../templates/pageTemplate'
+import { translate } from 'react-i18next'
+import CenteredButton from '../components/CenteredButton'
+
+const ChamberChoirPage = ({ t }) => (
+  <PageTemplate title={t('chamber-choir.title')}>
+    <Markdown>{t('chamber-choir.text')}</Markdown>
+    <CenteredImage
+      url="https://farm8.staticflickr.com/7341/16381074780_74ff518b92_z.jpg"
+      title={t('chamber-choir.title')}
+    />
+  </PageTemplate>
+)
+
+export default translate('translations')(ChamberChoirPage)
