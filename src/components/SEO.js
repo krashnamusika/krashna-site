3e1bacaac38b150dbb13a34b050567ed348ac936
Georgios Andreadis <g.andreadis@student.tudelft.nl>
2018-03-08 00:03:33 +0100

    Add SEO logo link

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {Helmet} from "react-helmet";
-import Favicon from '../layouts/favicon.png';
 
 const SEO = ({ title, description, url }) => (
   <Helmet>
@@ -12,13 +11,13 @@ const SEO = ({ title, description, url }) => (
     <meta property="og:url" content={url} />
     <meta property="og:title" content={title} />
     <meta property="og:description" content={description} />
-    <meta property="og:image" content={Favicon} />
+    <meta property="og:image" content="https://www.krashna.nl/krashna-logo.jpg" />
 
     {/* Twitter Card tags */}
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:title" content={title} />
     <meta name="twitter:description" content={description} />
-    <meta name="twitter:image" content={Favicon} />
+    <meta name="twitter:image" content="https://www.krashna.nl/krashna-logo.jpg" />
   </Helmet>
 )
 
