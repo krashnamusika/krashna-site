7dba1fe22c4413735c2b84b5c424b8f67397fdc2
MathiasMeuleman <m.meuleman@student.tudelft.nl>
2019-02-27 09:55:43 +0100

    Add texts to open rehearsal banner and page

--- src/components/IndexPage/OpenRehearsalsSection.js
+++ src/components/IndexPage/OpenRehearsalsSection.js
@@ -7,18 +7,18 @@ const OpenRehearsalsSection = ({ rehearsals, t }) => {
     <div className="bg-light-red">
       <div className="container pt-5 pb-5">
         <h2 className="text-center mb-3">
-          {t('index.open-rehearsals.title')}
+          {t('open-rehearsals.banner.title')}
         </h2>
         <div className="row mb-2">
           <div className="col-lg-11 col-sm-10 col-9">
             <h5 className="font-weight-bold">
-              {t('index.open-rehearsals.text-orchestra')}
+              {t('open-rehearsals.banner.text')}
             </h5>
           </div>
         </div>
         <div className="text-center">
-          <Link className="btn btn-danger" to="/all-concerts">
-            {t('index.open-rehearsals.more')}
+          <Link className="btn btn-danger" to="/open-rehearsals">
+            {t('open-rehearsals.banner.more')}
             <span className="fa fa-caret-right ml-2" />
           </Link>
         </div>
--- src/pages/open-rehearsals.js
+++ src/pages/open-rehearsals.js
@@ -4,8 +4,8 @@ import Markdown from 'react-remarkable'
 import PageTemplate from '../templates/pageTemplate'
 
 const OpenRehearsalsPage = ({ t }) => (
-  <PageTemplate title={t('open-rehearsals.title')}>
-    <Markdown>{t('open-rehearsals.text')}</Markdown>
+  <PageTemplate title={t('open-rehearsals.page.title')}>
+    <Markdown>{t('open-rehearsals.page.text')}</Markdown>
   </PageTemplate>
 )
 
