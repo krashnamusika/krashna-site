408c05ae90276a83c26d55e6816428cdf25c6b32
Georgios Andreadis <G.Andreadis@student.tudelft.nl>
2019-04-16 20:37:27 +0200

    Move Sponsorkliks logo and change sponsor text

--- src/components/IndexPage/TwoPartLayout.js
+++ src/components/IndexPage/TwoPartLayout.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { injectIntl } from 'gatsby-plugin-intl'
 
-const TowPartLayout = ({ title, children }) => {
+const TowPartLayout = ({ title, titleLeft, children }) => {
   return (
     <div className="row">
-      <h2 className="col-md-8 text-md-left align-self-center">{title}</h2>
-      <div className="col-md-4 text-md-right align-self-center">{children}</div>
+      <h2 className={`align-self-center col-md-8 ${titleLeft ? "text-md-left order-1" : "text-md-right order-2"}`}>{title}</h2>
+      <div className={`align-self-center col-md-4 ${titleLeft ? "text-md-right order-2" : "text-md-left order-1"}`}>{children}</div>
     </div>
   )
+}
+
+TowPartLayout.defaultProps = {
+  titleLeft: true
 }
 
 export default injectIntl(TowPartLayout)
