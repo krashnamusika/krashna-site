310ada05478329aec5149ebe7f92ed7c6b648650
Georgios Andreadis <G.Andreadis@student.tudelft.nl>
2019-04-24 11:19:10 +0200

    Add Tikkie links

--- src/components/DaanDonationModule.js
+++ src/components/DaanDonationModule.js
@@ -7,19 +7,25 @@ const DaanDonationModule = ({ intl, moreInformation, buttonDark }) => (
     <Markdown>{intl.formatMessage({ id: 'daan.text' })}</Markdown>
     <div className="text-center">
       <a
-        href="https://krashna.nl"
+        href="https://tikkie.me/pay/bgmgv59eogr4ml9711e1"
+        className={`btn btn-outline-${buttonDark ? 'dark' : 'light'} mr-2`}
+      >
+        10€
+      </a>
+      <a
+        href="https://tikkie.me/pay/vsog4a3sndk570eb8iau"
         className={`btn btn-outline-${buttonDark ? 'dark' : 'light'} mr-2`}
       >
         25€
       </a>
       <a
-        href="https://krashna.nl"
+        href="https://tikkie.me/pay/gm8hgukp4opoukevck0i"
         className={`btn btn-outline-${buttonDark ? 'dark' : 'light'} mr-2`}
       >
         50€
       </a>
       <a
-        href="https://krashna.nl"
+        href="https://tikkie.me/pay/tkr1jovt0ob5mddp72qg"
         className={`btn btn-outline-${buttonDark ? 'dark' : 'light'}`}
       >
         100€
