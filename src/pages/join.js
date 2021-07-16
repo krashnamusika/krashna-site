7b13124bc06e4f11e621a12d8a24d481447382da
Okaaij <33690953+OttoKaaij@users.noreply.github.com>
2021-07-16 10:35:52 +0200

    Prep owee 2021 interest list (#68)

--- src/pages/join.js
+++ src/pages/join.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import { injectIntl } from 'gatsby-plugin-intl'
-import Markdown from 'react-remarkable'
-import PageTemplate from '../templates/pageTemplate'
+import React from "react";
+import { injectIntl } from "gatsby-plugin-intl";
+import Markdown from "react-remarkable";
+import PageTemplate from "../templates/pageTemplate";
 
 const JoinPage = ({ intl }) => (
-  <PageTemplate title={intl.formatMessage({ id: 'join.title' })}>
-    <Markdown>{intl.formatMessage({ id: 'join.text' })}</Markdown>
+  <PageTemplate title={intl.formatMessage({ id: "join.title" })}>
+    <div className="text-center pt-3 pb-3">
+      <a
+        className="btn btn-primary btn-lg"
+        href="https://forms.gle/B5Fgyp4vqQtMyZWQ9"
+        target="blank"
+      >
+        <span className="fa fa-pencil mr-2" />
+        {intl.formatMessage({ id: "owee.form" })}
+      </a>
+    </div>
+    <Markdown>{intl.formatMessage({ id: "join.text" })}</Markdown>
   </PageTemplate>
-)
+);
 
-export default injectIntl(JoinPage)
+export default injectIntl(JoinPage);
